32e13fb0d94e44d87a88e2d3b974de263081dcc2
RealAngeleno <119266468+RealAngeleno@users.noreply.github.com>
2023-01-06 00:02:09 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+$website = "";
 $current_url = $_SERVER['REQUEST_URI'];
 $url_components = parse_url($current_url);
 
@@ -11,28 +12,26 @@ if (isset($url_components['query'])) {
 $website = filter_var($website, FILTER_SANITIZE_URL);
 $website_host = parse_url($website, PHP_URL_HOST);
 
-//Domain blocking. Only include the domain. (e.g. example.com NOT https://example.com) $blocklist = ["example.com", "google.com"];
+//Domain blocking. Only include the domain. (e.g. example.com NOT https://example.com)
 $blocklist = [""];
-//URL blocking. http://example.com/test and https://example.com/test are seen as different URLs. Follows same form as $blocklist, except with URLs.
+//URL blocking. http://example.com/test and https://example.com/test are seen as different URLs.
 $blockurl = [""];
-//Skiplist. (have the jump script immediately redirect to domains listed here) Follows the exact same form as $blocklist.
-$skiplist = [""];
+//Skiplist. (have the jump script immediately redirect to domains listed here ) Only include the domain, not https://.
+$skiplist = ["world2ch.net"];
 
 if ($website == NULL) {
     die("No Link Specified");
 }
-//Die if the url doesn't start with http://, https://, or mailto:
 elseif (strpos($website, "https://") !== 0 && strpos($website, "http://") !== 0 && strpos($website, "mailto:") !== 0) {
-    die("Invalid URL");
+    echo("Invalid URL");
+    die();
 }
-//Skip immediately to target domain ($skiplist)
 elseif (in_array($website_host, $skiplist)) {
     die("Quickly jumping to the following link: <a href='" . htmlspecialchars($website, ENT_QUOTES) . "'>" . htmlspecialchars($website, ENT_QUOTES) . "</a><meta http-equiv='refresh' content='0; URL=$website'>");
-//Domain Blocking
+}
 elseif (in_array($website_host, $blocklist)) {
     die("Sorry, the domain you are attempting to jump to is blocked.");
 }
-//URL Blocking
 elseif (in_array($website, $blockurl)) {
     die("Sorry, the URL you are attempting to jump to is blocked.");
     }
